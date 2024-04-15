386beb2571be7fcd18c87978d75b930ab20e5336
shrinivassab <shrinivassab@gmail.com>
2024-04-14 21:04:33 -0400

    UI Done

--- script.js
+++ script.js
@@ -13,7 +13,45 @@ function returnMovies(url) {
     .then(function(data){
         console.log(data.results)
         data.results.forEach(element => {
-            const div = document.createElement('div')
+            const div_card = document.createElement('div')
+            div_card.setAttribute('class', 'card')
+
+            const div_row = document.createElement('div')
+            div_row.setAttribute('class', 'row')
+
+            const div_column = document.createElement('div')
+            div_column.setAttribute('class', 'column')
+
+            const image = document.createElement('img')
+            image.setAttribute('class', 'thumbnail')
+            image.setAttribute('id', 'image')
+
+            const title = document.createElement('h3')
+            title.setAttribute('id', 'title')
+
+            const center = document.createElement('center')
+
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+            center.appendChild(image)
+            div_card.appendChild(center)
+            div_card.appendChild(title)
+            div_column.appendChild(div_card)
+            div_row.appendChild(div_column)
+
+            main.appendChild(div_row)
         });
     })
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    main.innerHTML = ""
+
+    const searchItem = search.value
+
+    if (search) {
+        returnMovies(SEARCHAPI + searchItem)
+        search.value = ""
+    }
+})
